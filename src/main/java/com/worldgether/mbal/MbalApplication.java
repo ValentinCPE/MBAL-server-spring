020631d9e424cf949f366667e311a6a3a816e5c1
Valentin Guevara <valentin.guevara@etu.univ-lyon1.fr>
2018-01-31 13:51:15 +0100

    first commit

---
+++ src/main/java/com/worldgether/mbal/MbalApplication.java
@@ -0,0 +1,12 @@
+package com.worldgether.mbal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbalApplication.class, args);
+	}
+}
